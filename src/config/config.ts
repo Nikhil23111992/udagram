1b6e70f100427bc9cfd58d9ec5c0c4efa9335003
nchaurasia <nchaurasia@manh.com>
2020-11-04 15:38:07 +0530

    Initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "udagramnik",
+    "password": "udagramnik",
+    "database": "udagramnik",
+    "host": "udagramnik.cg4owykb6muj.ap-south-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "ap-south-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-nik-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  },
+  "jwt":{
+    "secret": "nikhilworld"
+  }
+}
